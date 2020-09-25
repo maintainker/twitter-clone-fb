b97735dfac9b17735f4a019b02bb43f2a79b1fca
yhsul <yhsul@nshc.net>
2020-09-25 18:01:24 +0900

    akanfl 3.2 듣는중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState, useEffect } from 'react';
 
-const Home = () => <span>Home </span>
+const Home = () =>{
+    const [nweet, setNweet] = useState("");
+    const [nweets,setNweets] = useState([]);
+    const getNweets = async () =>{
+        
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach(doc=>{
+            const nweets
+            setNweets(prev => [doc.data(),...prev]);
+        });
+    }
+    useEffect(() => {
+        getNweets();
+    }, [])
+    const onSubmit = async (e) =>{
+        e.preventDefault();
+        await dbService.collection("nweets").add({
+        nweet,
+        createdAt : Date.now(),
+        });
+        setNweet("")
+    }
+    const onChange = (e) =>{
+        const {target:{value}} = e;
+        setNweet(value);
+    }
+    console.log(nweets);
+    return ( <div>
+        <form onSubmit={onSubmit}>
+            <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
+            <input type="submit" value="Nweet"/>
+        </form>
+    </div>)}
 
 
  export default Home;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,14 @@
+import { authService } from 'fbase';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default () => <span>Profile </span>
+export default () => {
+    const histroy = useHistory ();
+    const onLogOutClick = () => {
+        authService.signOut();
+        histroy.push("/");
+    };
+    return <>
+        <button onClick={onLogOutClick}>Log Out</button>
+    </>
+}
